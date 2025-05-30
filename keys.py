d03134e769df30809cee5a186d60f5405e93707e
lqhac <113124029+lqhac@users.noreply.github.com>
2025-05-30 19:32:54 +0800

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,12 +1,12 @@
 src_keys = ['strength', 'length', 'phrase']
 tgt_keys = ['bar', 'pos', 'token', 'dur', 'phrase']
 
-binary_dir = '/home/qihao/CS6207/binary'
-words_dir = '/home/qihao/CS6207/binary/words'
+binary_dir = './binary'
+words_dir = './binary/words'
 
 hparams = {
     'batch_size': 10,
-    'word_data_dir': '/home/qihao/CS6207/binary/words',
+    'word_data_dir': './binary/words',
     'sentence_maxlen': 512,
     'hidden_size': 768,
 }
